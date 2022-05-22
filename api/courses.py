83ba4b75b7944e668de384533b0ffc2a6e5e42c2
jarasyola <jamraph10@gmail.com>
2022-05-22 12:09:05 +0300

    6-course_api

--- api/courses.py
+++ api/courses.py
@@ -1,31 +1,47 @@
+
+from typing import List
+
 import fastapi
+from fastapi import Depends, HTTPException
+from sqlalchemy.orm import Session
 
-router = fastapi.APIRouter(
-    prefix='/courses',
-    tags=['Courses']
-)
+from db.db_setup import get_db
+from pydantic_schemas.course import Course, CourseCreate
+from api.utils.courses import get_course, get_courses, create_course
 
-@router.get("/")
-async def read_courses():
-    return {"courses": []}
+router = fastapi.APIRouter()
 
 
-@router.post("/")
-async def create_course_api():
-    return {"courses": []}
+@router.get("/courses", response_model=List[Course])
+async def read_courses(db: Session = Depends(get_db)):
+    courses = get_courses(db=db)
+    return courses
 
-@router.get("/:{id}")
-async def read_course():
-    return {"courses": []}
 
-@router.patch("/:{id}")
+@router.post("/courses", response_model=Course)
+async def create_new_course(course: CourseCreate, db: Session = Depends(get_db)):
+    return create_course(db=db, course=course)
+
+
+@router.get("/courses/{course_id}")
+async def read_course(course_id: int, db: Session = Depends(get_db)):
+    db_course = get_course(db=db, course_id=course_id)
+    if db_course is None:
+        raise HTTPException(status_code=404, detail="Course not found")
+    return db_course
+
+
+@router.patch("/courses/{course_id}")
 async def update_course():
     return {"courses": []}
 
-@router.delete("/:{id}")
+
+@router.delete("/courses/{course_id}")
 async def delete_course():
     return {"courses": []}
 
-@router.get("/:{id}/sections")
+
+@router.get("/courses/{course_id}/sections")
 async def read_course_sections():
     return {"courses": []}
+    
--- api/users.py
+++ api/users.py
@@ -6,8 +6,10 @@ from sqlalchemy.orm import Session
 
 from db.db_setup import get_db
 from pydantic_schemas.user import UserCreate, User
-
+from pydantic_schemas.course import Course
 from api.utils.users import get_user, get_user_by_email, get_users, create_user
+from api.utils.courses import get_user_courses
+
 
 router = fastapi.APIRouter(
      prefix='/users',
@@ -16,13 +18,13 @@ router = fastapi.APIRouter(
 
 
 
-@router.get("/users", response_model=List[User])
+@router.get("/", response_model=List[User])
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = get_users(db, skip=skip, limit=limit)
     return users
 
 
-@router.post("/users", response_model=User, status_code=201)
+@router.post("/", response_model=User, status_code=201)
 async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     db_user = get_user_by_email(db=db, email=user.email)
     if db_user:
@@ -30,9 +32,15 @@ async def create_new_user(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db=db, user=user)
 
 
-@router.get("/users/{user_id}", response_model=User)
+@router.get("/{user_id}", response_model=User)
 async def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = get_user(db=db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.get("/{user_id}/courses", response_model=List[Course])
+async def read_user_courses(user_id: int, db: Session = Depends(get_db)):
+    courses = get_user_courses(user_id=user_id, db=db)
+    return courses
